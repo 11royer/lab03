f4286a1757346fd6c02befb4626dfa44fb347989
royer <royermartinezperez@gmail.com>
2024-03-21 23:34:02 -0300

    actualizacion

---
+++ src/P-05.js
@@ -0,0 +1,20 @@
+function sortFruits(fruits) {
+	return fruits.sort();
+} 
+
+function getElementInTheMiddle(list) {
+	return list[list.length / 2];
+}
+
+function adminLogin(user) {
+	if(user == "ADMIN") return true;
+	if(user == "DEV") return true;
+	return false;
+}
+
+
+const sortFruits = (fruits) => fruits.sort();
+
+const getElementInTheMiddle = (list) => list[list.length / 2];
+
+const adminLogin = (user) => user === "ADMIN" || user === "DEV";
